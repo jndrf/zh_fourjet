04ee90986a4eeff27fa31177fe914dcb49df503e
Jonas Neundorf <jneun1@web.de>
2017-11-28 19:51:51 +0100

    fixed typo

--- TreeProducer.py
+++ TreeProducer.py
@@ -53,7 +53,7 @@ class TreeProducer(Analyzer):
         bookJet(self.tree, 'rescaled_jet3', self.taggers)
         bookJet(self.tree, 'rescaled_jet4', self.taggers)
 
-        Bookparticle(self.tree, 'misenergy')
+        BookParticle(self.tree, 'misenergy')
         bookParticle(self.tree, 'higgs')
         bookParticle(self.tree, 'zed')
         bookLepton(self.tree, 'lepton1')
